234036e9a0d336665e748ed7c32862cb5a9922c9
smsarfroz <72331821+smsarfroz@users.noreply.github.com>
2022-06-27 14:56:05 +0530

    Create detailed_template.cpp

---
+++ detailed_template.cpp
@@ -0,0 +1,270 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include<iostream>
+#include<sstream>
+#include<set>
+#include<map>
+#include<queue>
+#include<bitset>
+#include<vector>
+#include<limits.h>
+#include<assert.h>
+#include <chrono>
+#include <random>
+using namespace std;
+using ll = long long;
+using ld = long double;
+#define mp   make_pair
+#define pb   push_back
+#define eb   emplace_back 
+#define ar   array
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+ 
+template <typename num_t>
+using ordered_set = tree<num_t, null_type, less_equal<num_t>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+template<typename T>
+using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename U> void amin(T& a, U b){if (a > b) a = b;}
+template<typename T, typename U> void amax(T& a, U b){if (a < b) a = b;}
+
+#define rep(i,j, k)    for(ll i = j; i < (k); ++i)
+#define repd(i, a, n)  for(int i = a; i >= (n); --i)
+#define ms0(X) memset((X), 0, sizeof((X)))
+#define ms1(X) memset((X), -1, sizeof((X)))
+#define fi first
+#define se second
+#define vc vector
+#define vi vc<int>
+#define sz(x) (int)x.size()
+#define all(x) (x).begin(),(x).end()
+#define rall(v) v.rbegin(),v.rend()
+#define pii  pair<int,int>
+#define getunique(v) {sort(all(v)); v.erase(unique(all(v)), v.end());
+#define sum(a)     ( accumulate ((a).begin(), (a).end(), 0ll))
+#define mine(a)    (*min_element((a).begin(), (a).end()))
+#define maxe(a)    (*max_element((a).begin(), (a).end()))
+#define mini(a)    ( min_element((a).begin(), (a).end()) - (a).begin())
+#define maxi(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
+#define lowb(a, x) ( lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
+#define uppb(a, x) ( upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
+#define nl "\n"
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+inline int64_t random_long(long long l = LLONG_MIN,long long r = LLONG_MAX){
+    uniform_int_distribution<int64_t> generator(l,r);
+    return generator(rng);
+}
+
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0); 
+    if (sz(name)) {
+        freopen((name+".in").c_str(), "r", stdin); 
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+struct custom_hash { // Credits: https://codeforces.com/blog/entry/62393
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+    template<typename L, typename R>
+    size_t operator()(pair<L,R> const& Y) const{
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(Y.first * 31 + Y.second + FIXED_RANDOM);
+    }
+};
+
+// usage-->  gp_hash_table<int,int,custom_hash> mep   
+
+//<-------------------------------modint----------------------------------->//
+template<const int MOD>
+struct _m_int {
+    int val;
+
+    _m_int(int64_t v = 0) {
+        if (v < 0) v = v % MOD + MOD;
+        if (v >= MOD) v %= MOD;
+        val = int(v);
+    }
+
+    _m_int(uint64_t v) {
+        if (v >= MOD) v %= MOD;
+        val = int(v);
+    }
+
+    _m_int(int v) : _m_int(int64_t(v)) {}
+    _m_int(unsigned v) : _m_int(uint64_t(v)) {}
+
+    static int inv_mod(int a, int m = MOD) {
+        // https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Example
+        int g = m, r = a, x = 0, y = 1;
+
+        while (r != 0) {
+            int q = g / r;
+            g %= r; swap(g, r);
+            x -= q * y; swap(x, y);
+        }
+
+        return x < 0 ? x + m : x;
+    }
+
+    explicit operator int() const { return val; }
+    explicit operator unsigned() const { return val; }
+    explicit operator int64_t() const { return val; }
+    explicit operator uint64_t() const { return val; }
+    explicit operator double() const { return val; }
+    explicit operator long double() const { return val; }
+
+    _m_int& operator+=(const _m_int &other) {
+        val -= MOD - other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+
+    _m_int& operator-=(const _m_int &other) {
+        val -= other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif
+        // Optimized mod for Codeforces 32-bit machines.
+        // x must be less than 2^32 * m for this to work, so that x / m fits in an unsigned 32-bit int.
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x);
+        unsigned quot, rem;
+        asm("divl %4\n"
+            : "=a" (quot), "=d" (rem)
+            : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+
+    _m_int& operator*=(const _m_int &other) {
+        val = fast_mod(uint64_t(val) * other.val);
+        return *this;
+    }
+
+    _m_int& operator/=(const _m_int &other) {
+        return *this *= other.inv();
+    }
+
+    friend _m_int operator+(const _m_int &a, const _m_int &b) { return _m_int(a) += b; }
+    friend _m_int operator-(const _m_int &a, const _m_int &b) { return _m_int(a) -= b; }
+    friend _m_int operator*(const _m_int &a, const _m_int &b) { return _m_int(a) *= b; }
+    friend _m_int operator/(const _m_int &a, const _m_int &b) { return _m_int(a) /= b; }
+
+    _m_int& operator++() {
+        val = val == MOD - 1 ? 0 : val + 1;
+        return *this;
+    }
+
+    _m_int& operator--() {
+        val = val == 0 ? MOD - 1 : val - 1;
+        return *this;
+    }
+
+    _m_int operator++(int) { _m_int before = *this; ++*this; return before; }
+    _m_int operator--(int) { _m_int before = *this; --*this; return before; }
+
+    _m_int operator-() const {
+        return val == 0 ? 0 : MOD - val;
+    }
+
+    friend bool operator==(const _m_int &a, const _m_int &b) { return a.val == b.val; }
+    friend bool operator!=(const _m_int &a, const _m_int &b) { return a.val != b.val; }
+    friend bool operator<(const _m_int &a, const _m_int &b) { return a.val < b.val; }
+    friend bool operator>(const _m_int &a, const _m_int &b) { return a.val > b.val; }
+    friend bool operator<=(const _m_int &a, const _m_int &b) { return a.val <= b.val; }
+    friend bool operator>=(const _m_int &a, const _m_int &b) { return a.val >= b.val; }
+
+    _m_int inv() const {
+        return inv_mod(val);
+    }
+
+    _m_int pow(int64_t p) const {
+        if (p < 0)
+            return inv().pow(-p);
+
+        _m_int a = *this, result = 1;
+
+        while (p > 0) {
+            if (p & 1)
+                result *= a;
+
+            p >>= 1;
+
+            if (p > 0)
+                a *= a;
+        }
+
+        return result;
+    }
+
+    friend ostream& operator << (ostream &os, const _m_int &m) { return os << m.val; }
+    friend istream& operator >> (istream& is, _m_int &m) {is >> m.val; m.val %= MOD; return is;}
+};
+//<-------------------------------modint----------------------------------->//
+const int MOD=1e9+7;
+using mint = _m_int<MOD>;
+const long long inf=1e18;
+const int32_t MM=998244353;
+//<-------------------------------debug----------------------------------->//
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+void __print(const mint &x) { cerr << x.val;}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(const T &t,const V &... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+//<-------------------------------debug----------------------------------->//
+const int N=2e5+10;
+
+void solve(){
+    
+}  
+signed main(){
+    setIO();
+    int t=1;
+    //cin >> t;
+    while(t--) solve();
+    return 0;
+}
+/*
+
+*/
